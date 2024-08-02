fc970ea5121c538997ad676c43129c76f84cd663
Bianca Venturin <fixed-term.bianca.venturin@br.bosch.com>
2024-08-02 14:12:58 -0300

    :sparkles: feat: style of the scroll and fixed img responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 export function Home() {
   return (
     <div className="flex justify-center">
-      <h1 className="absolute text-white text-5xl mt-10">Welcome to the Potterpedia!</h1>
-      <img className="w-full h-full" src="../home_background.png"/>
+      <h1 className="absolute z-10 text-white text-5xl mt-10 select-none">Welcome to the Potterpedia!</h1>
+      <img className="fixed pointer-events-none select-none w-full h-full object-cover" src="../home_background.png"/>
     </div>
   );
 }
